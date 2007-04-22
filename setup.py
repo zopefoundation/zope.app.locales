503950305d739f8fb9da1ba9b77dd237e92b0c0e
Christian Theune <ct@gocept.com>
2007-04-22 10:39:10 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.locales',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.app.locales',
       license='ZPL 2.1',
       description='Zope locales',
