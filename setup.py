61367a819639d282abdf0932574a2633e42d7607
Christophe Combelles <ccomb@free.fr>
2007-12-12 00:15:02 +0000

    Next version is at least 3.4.2

--- setup.py
+++ setup.py
@@ -22,14 +22,14 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.locales',
-      version = '3.4.1dev',
+      version = '3.4.2dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope locale extraction and management utilities',
       long_description=(
           read('README.txt')
           + '\n\n' +
-          'Detailed Dcoumentation\n' +
+          'Detailed Documentation\n' +
           '----------------------\n'
           + '\n\n' +
           read('src', 'zope', 'app', 'locales', 'TRANSLATE.txt')
