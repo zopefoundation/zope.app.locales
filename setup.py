fe8bdc59ffe4bead1e18f655608ffbc4fa3e306c
Dmitry Vasiliev <dima@hlabs.spb.ru>
2008-03-05 05:28:42 +0000

    Updated release version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.locales',
-      version = '3.4.4dev',
+      version = '3.4.4',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope locale extraction and management utilities',
