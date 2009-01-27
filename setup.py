020053df9189578540128dcff2b627c5f3c6b8a8
Wolfgang Schnerring <wosc@wosc.de>
2009-01-27 13:40:42 +0000

    released 3.5.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.locales',
-      version = '3.5.1dev',
+      version = '3.5.1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope locale extraction and management utilities',
