8767ea4b337596dcbe25cef96ecc666d794477fd
Michael Howitz <mh@gocept.com>
2016-03-28 09:22:55 +0200

    PEP-8

--- setup.py
+++ setup.py
@@ -16,33 +16,29 @@
 # When developing and releasing this package, please follow the documented
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
-"""Setup for zope.app.locales package
-
-$Id$
-"""
+"""Setup for zope.app.locales package"""
 import os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 
 setup(name='zope.app.locales',
       version='3.7.5dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope locale extraction and management utilities',
-      long_description=(
-          read('README.txt')
-          + '\n\n' +
-          'Detailed Documentation\n' +
-          '----------------------\n'
-          + '\n\n' +
-          read('src', 'zope', 'app', 'locales', 'TRANSLATE.txt')
-          + '\n\n' +
-          read('CHANGES.txt')
-          ),
-      keywords = "zope3 i18n l10n translation gettext",
-      classifiers = [
+      long_description='\n\n'.join([
+          read('README.txt'),
+          'Detailed Documentation\n'
+          '----------------------',
+          read('src', 'zope', 'app', 'locales', 'TRANSLATE.txt'),
+          read('CHANGES.txt'),
+      ]),
+      keywords="zope3 i18n l10n translation gettext",
+      classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Web Environment',
           'Intended Audience :: Developers',
@@ -56,31 +52,31 @@ setup(name='zope.app.locales',
       url='http://pypi.python.org/pypi/zope.app.locales',
       license='ZPL 2.1',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['zope', 'zope.app'],
       install_requires=['setuptools',
                         'zope.i18nmessageid',
                         'zope.interface',
                         ],
-      extras_require = dict(
+      extras_require=dict(
           test=[
               'zope.i18n',
               'zope.security',
               'zope.tal',
               'zope.testing',
-                ],
+          ],
           zcml=[
               'zope.i18n',
               'zope.configuration',
-              ],
+          ],
           extract=[
               'zope.tal',
-              ],
-          ),
-      include_package_data = True,
-      zip_safe = False,
+          ],
+      ),
+      include_package_data=True,
+      zip_safe=False,
       entry_points="""
       [console_scripts]
-      i18nextract = zope.app.locales.extract:main [extract]
+      i18nextract=zope.app.locales.extract:main [extract]
       """
       )
