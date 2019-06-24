111e988640241b8bcbff1e5bdbd4aba5e92e9f47
Michael Howitz <mh@gocept.com>
2019-06-24 17:56:09 +0200

    Back to development: 4.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.locales',
-      version='4.1',
+      version='4.2.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope locale extraction and management utilities',
