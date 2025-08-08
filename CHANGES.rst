cca4f20c44848dc24ceafc7dff4f10dd4aec4c9a
Michael Howitz <icemac@gmx.net>
2025-08-08 08:33:23 +0200

    Bumped version for breaking release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
 CHANGES
 =======
 
-5.2 (unreleased)
+6.0 (unreleased)
 ----------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.locales',
-      version='5.2.dev0',
+      version='6.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope locale extraction and management utilities',
